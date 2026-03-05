13cb49a758fa275a21921a3c381dec996875e662
Pavankalyangelli1 <77654354+Pavankalyangelli1@users.noreply.github.com>
2026-03-05 21:01:14 +0530

    Update gold.py

--- gold.py
+++ gold.py
@@ -18,8 +18,8 @@ def send_telegram(message):
     requests.post(url, data=data)
 
 def fetch_prices():
-    gold_usd = yf.Ticker("GC=F").history(period="1d")["Close"].iloc[-1]
-    silver_usd = yf.Ticker("SI=F").history(period="1d")["Close"].iloc[-1]
+    gold_usd = yf.Ticker("GOLDBEES.NS").history(period="1d")["Close"].iloc[-1]
+    silver_usd = yf.Ticker("SILVERBEES.NS").history(period="1d")["Close"].iloc[-1]
     usd_inr = yf.Ticker("USDINR=X").history(period="1d")["Close"].iloc[-1]
     gold_inr_per_ounce  = gold_usd * usd_inr
     silver_inr_per_ounce  = silver_usd * usd_inr
@@ -75,7 +75,7 @@ last_gold_price = None
 
 while True:
     try:
-        gold24, gold22, silver, vijayawada, chennai_silver = fetch_prices()
+        gold24, gold22, silver, Vijayawada, chennai_silver = fetch_prices()
 
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -83,16 +83,16 @@ while True:
         print(f"Gold 24K: ₹{gold24:.2f} /10g")
         print(f"Gold 22K: ₹{gold22:.2f} /10g")
         print(f"Silver: ₹{silver:.2f} /kg")
-        print(f"Vijayawada Est 22K Jewellery: ₹{vijayawada:.2f} /10g")
-        print(f"Vijayawada Silver Jewellery Est: ₹{chennai_silver:.2f} /kg")
+        print(f"Vijayawada Est 22K Jewellery: ₹{Vijayawada:.2f} /10g")
+        print(f"Vijayawada Silver Jewellery Est: ₹{Vijayawada_silver:.2f} /kg")
 
         data = {
             "Time": now,
             "Gold_24K": round(gold24,2),
             "Gold_22K": round(gold22,2),
             "Silver": round(silver,2),
-            "Chennai_22K_Est": round(vijayawada,2),
-            "Chennai_Silver_Est": round(chennai_silver,2)
+            "Vijayawada_22K_Est": round(Vijayawada,2),
+            "Vijayawada_Silver_Est": round(Vijayawada_silver,2)
         }
 
         save_to_excel(data)
